7a82a2edd63fa71041f86386bc1294cb7aa34cbb
felimomo <felimomouni@gmail.com>
2026-09-16 23:08:15 -0600

    reproduction script: get data

--- reproduce.sh
+++ reproduce.sh
@@ -1,6 +1,7 @@
 conda env create -f environment.yml -n verify-repro
 conda activate verify-repro
 python -m ipykernel install --user --name verify-repro
+python get-data.py
 jupyter nbconvert --execute --to notebook \
 	--inplace \
 	--ExecutePreprocessor.kernel_name=verify-repro analysis.ipynb
